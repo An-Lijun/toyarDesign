ec4e18331bd7bb8ae3efca3954825c278ee17035
yutian <nj_yutian@qq.com>
2024-04-20 10:16:45 +0800

    uper breadcrumb

---
+++ src/package/components/breadcrumb/src/breadcrumb.vue
@@ -0,0 +1,10 @@
+<template>
+  <div :class="[nm.b()]">
+  </div>
+</template>
+<script setup>
+import { nm } from './context.ts'
+
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/package/components/breadcrumb/src/context.ts
@@ -0,0 +1,6 @@
+import useNmSpace from '../../../hooks/useBem'
+
+export const nm = useNmSpace('breadcrumb')
+
+export const nmI = useNmSpace('breadcrumb-item')
+
