8012e27eeb1e7cd34b0d37740a1748a07259e6dc
yutian <nj_yutian@qq.com>
2024-10-26 20:55:11 +0800

    回退config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { join } from "path";
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'//setUp name
 import autoImport from 'unplugin-auto-import/vite'//引入语法
-import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
@@ -22,21 +22,20 @@ export default defineConfig({
       input: {
         main: join(__dirname, 'src/package/index.ts'),
         icon:join(__dirname, 'src/package/icon.ts'),
-        module:join(__dirname, 'src/package/module.ts'),
       },
       output: {
         inlineDynamicImports:false,
         chunkFileNames: 'js/[name]-[hash].js',
-        // assetFileNames(assetInfo){
-        //   // /\.svg$/.test(assetInfo.name)
-        //   if (assetInfo.type === 'asset') { 
-        //     // 匹配资源文件后缀
-        //     // 创建media文件夹存放匹配的资源文件,name为该文件的原名，hash为哈希值，ext为文件后缀名，以[name].[hash][ext]命名规则
-        //     return `assets/[name].[hash][ext]`;  
-        //   }
-        //   // 不匹配的资源文件存放至assets，以[name]-[hash].[ext]命名规则，注意两处的命名规则不同
-        //   return `assets1/[name]-[hash].[ext]`; 
-        // },
+        assetFileNames(assetInfo){
+          // /\.svg$/.test(assetInfo.name)
+          if (assetInfo.type === 'asset') { 
+            // 匹配资源文件后缀
+            // 创建media文件夹存放匹配的资源文件,name为该文件的原名，hash为哈希值，ext为文件后缀名，以[name].[hash][ext]命名规则
+            return `assets/[name].[hash][ext]`;  
+          }
+          // 不匹配的资源文件存放至assets，以[name]-[hash].[ext]命名规则，注意两处的命名规则不同
+          return `assets1/[name]-[hash].[ext]`; 
+        },
         format: "es",
         globals: {
           vue: "Vue",
@@ -52,13 +51,5 @@ export default defineConfig({
       '@': join(__dirname, "src"),
     },
   },
-  plugins: [vue(), VueSetupExtend(), autoImport({ imports: ['vue'] }),chunkSplitPlugin({
-
-    customSplitting: {
-      // All files in `src/container` will be merged together in `container` chunk
-      // 'style1': [/\.css$/]
-      'component':[/\.vue/]
-
-    }
-  })],
+  plugins: [vue(), VueSetupExtend(), autoImport({ imports: ['vue'] })],
 })
