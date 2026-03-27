4d4f4641001c33ce70bc67259318440760b62e08
yutian <nj_yutian@qq.com>
2026-03-27 20:26:25 +0800

    [feat] 添加button 新类型

--- src/package/components/button/src/button.vue
+++ src/package/components/button/src/button.vue
@@ -1,19 +1,14 @@
 <template>
-  <button
-    :type="htmlType"
-    :class="[
-      nm.b(),
-      nm.m(state),
-      nm.m(type),
-      nm.m(size),
-      nm.m(shape),
-      nm.is('disabled', mergeDisabled),
-      nm.is('readonly', mergeReadonly),
-      nm.is('block', block)
-    ]"
-    :disabled="mergeDisabled"
-    :readonly="mergeReadonly"
-  >
+  <button :type="htmlType" :class="[
+    nm.b(),
+    nm.m(state),
+    nm.m(type),
+    nm.m(size),
+    nm.m(shape),
+    nm.is('disabled', mergeDisabled),
+    nm.is('readonly', mergeReadonly),
+    nm.is('block', block)
+  ]" :disabled="mergeDisabled" :readonly="mergeReadonly">
     <span>
       <span v-show="loading" :class="nm.is('loading', loading)">
         <TyiLoader2Line />
@@ -56,10 +51,12 @@ const mergeReadonly = computed(() => {
   0% {
     transform: translate(-50%, -50%) rotate(0deg);
   }
+
   100% {
     transform: translate(-50%, -50%) rotate(360deg);
   }
 }
+
 // ------------------------  按钮base样式  ------------------------
 .ty-button {
   display: inline-block;
@@ -77,19 +74,23 @@ const mergeReadonly = computed(() => {
   font-size: var(--font-body-3);
   border-radius: var(--border-radius-4);
   position: relative;
+
   // ------------------------  按钮状态样式  ------------------------
   @mixin addBtnState($state) {
+
     //基础按钮
     &--normal.ty-button--#{$state} {
       color: #fff;
       background-color: var(--#{$state}-6);
       border-color: var(--#{$state}-6);
+
       &:hover,
       &:focus {
         background: var(--#{$state}-5);
         border-color: var(--#{$state}-5);
         color: #fff;
       }
+
       &:active {
         border-color: var(--#{$state}-7);
         background-color: var(--#{$state}-7);
@@ -98,6 +99,7 @@ const mergeReadonly = computed(() => {
       //基础按钮的禁用样式
       &.is-readonly,
       &.is-disabled {
+
         &,
         &:hover,
         &:focus,
@@ -111,7 +113,28 @@ const mergeReadonly = computed(() => {
     }
 
     //次级按钮
+
     &--secondary.ty-button--#{$state} {
+      background-color: var(--#{$state}-1);
+      color: var(--#{$state}-6);
+      border-color: var(--#{$state}-1);
+
+      &:hover,
+      &:focus {
+        background:  var(--#{$state}-2);
+        color: var(--#{$state}-6);
+        border-color: var(--#{$state}-2);
+      }
+
+      &:active {
+        background-color: var(--#{$state}-6);;
+        color: var(--#{$state}-7);
+        border-color: var(--#{$state}-6);
+      }
+    }
+
+    // outline
+    &--outline.ty-button--#{$state} {
       background-color: unset;
       color: var(--#{$state}-6);
       border-color: var(--#{$state}-6);
@@ -122,46 +145,54 @@ const mergeReadonly = computed(() => {
         color: var(--#{$state}-5);
         border-color: var(--#{$state}-5);
       }
+
       &:active {
         background-color: unset;
         color: var(--#{$state}-7);
         border-color: var(--#{$state}-7);
       }
     }
+
     // 虚线按钮
     &--dashed.ty-button--#{$state} {
       border: var(--border-1) dashed;
       background-color: unset;
       color: var(--#{$state}-6);
+
       &:hover,
       &:focus {
         background: unset;
         color: var(--#{$state}-5);
       }
+
       &:active {
         background-color: unset;
         color: var(--#{$state}-7);
       }
     }
+
     // 文字按钮
     &--text.ty-button--#{$state} {
       color: var(--#{$state}-6);
       border: unset;
       background: unset;
+
       &:hover,
       &:focus {
         background: unset;
         color: var(--#{$state}-5);
       }
+
       &:active {
         color: var(--#{$state}-7);
       }
     }
+
     //次级按钮 虚线按钮 文字按钮 的禁用状态
-    &--secondary.ty-button--#{$state}.is-readonly,
+    &--outline.ty-button--#{$state}.is-readonly,
     &--dashed.ty-button--#{$state}.is-readonly,
     &--text.ty-button--#{$state}.is-readonly,
-    &--secondary.ty-button--#{$state}.is-disabled,
+    &--outline.ty-button--#{$state}.is-disabled,
     &--dashed.ty-button--#{$state}.is-disabled,
     &--text.ty-button--#{$state}.is-disabled {
       &,
@@ -173,22 +204,40 @@ const mergeReadonly = computed(() => {
         border-color: var(--#{$state}-3);
       }
     }
+
+    &--secondary.ty-button--#{$state}.is-readonly,
+    &--secondary.ty-button--#{$state}.is-disabled {
+      &,
+      &:hover,
+      &:focus,
+      &:active {
+        color: var(--#{$state}-3);
+        cursor: no-drop;
+        border-color: var(--#{$state}-1);
+        background: var(--#{$state}-1);
+      }
+    }
+
     // 链接按钮
     &--link.ty-button--#{$state} {
       color: var(--#{$state}-6);
       border: unset;
       background: unset;
+
       span {
         border-bottom: var(--border-1) solid var(--#{$state}-6);
       }
+
       &:hover,
       &:focus {
         background: unset;
         color: var(--#{$state}-5);
+
         span {
           border-bottom: var(--border-1) solid var(--#{$state}-5);
         }
       }
+
       &:active {
         color: var(--#{$state}-7);
         span {
@@ -198,6 +247,7 @@ const mergeReadonly = computed(() => {
 
       &.is-readonly,
       &.is-disabled {
+
         &,
         &:hover,
         &:focus,
@@ -205,85 +255,82 @@ const mergeReadonly = computed(() => {
           color: var(--#{$state}-3);
           cursor: no-drop;
           border-color: var(--#{$state}-3);
+
           span {
             border-bottom: var(--border-1) solid var(--#{$state}-3);
           }
         }
       }
     }
+
     // 链接按钮 的禁用状态
   }
+
   @each $state in 'primary', 'link', 'success', 'warning', 'danger' {
     @include addBtnState($state);
   }
 
   // ------------------------  按钮圆角样式  ------------------------
   $btnShape: (
-    square: (
-      2
-    ),
-    round: (
-      16
-    ),
-    circle: (
-      circle
-    )
+    square: (2),
+    round: (16),
+    circle: (circle)
   );
 
-  @mixin addBtnShape($name, $value) {
-    &--#{$name} {
-      border-radius: var(--border-radius-#{$value});
-    }
+@mixin addBtnShape($name, $value) {
+  &--#{$name} {
+    border-radius: var(--border-radius-#{$value});
+  }
+}
+
+@each $name, $value in $btnShape {
+  @include addBtnShape($name, $value);
+}
+
+// ------------------------  按钮尺寸样式  ------------------------
+$btnSize: (
+  mini: (1),
+  small: (3),
+  medium: (3),
+  large: (4)
+);
+
+@mixin addBtnSize($name, $value) {
+  &--#{$name} {
+    height: var(--size-#{$name});
+    line-height: var(--size-#{$name});
+    padding: 0 var(--padding-#{$value});
   }
 
-  @each $name, $value in $btnShape {
-    @include addBtnShape($name, $value);
+  &--#{$name}.ty-button--circle {
+    height: var(--size-#{$name});
+    width: var(--size-#{$name});
+    padding: unset;
   }
+}
 
-  // ------------------------  按钮尺寸样式  ------------------------
-  $btnSize: (
-    mini: (
-      1
-    ),
-    small: (
-      3
-    ),
-    medium: (
-      3
-    ),
-    large: (
-      4
-    )
-  );
-  @mixin addBtnSize($name, $value) {
-    &--#{$name} {
-      height: var(--size-#{$name});
-      line-height: var(--size-#{$name});
-      padding: 0 var(--padding-#{$value});
-    }
-    &--#{$name}.ty-button--circle {
-      height: var(--size-#{$name});
-      width: var(--size-#{$name});
-      padding: unset;
-    }
-  }
-  @each $name, $value in $btnSize {
-    @include addBtnSize($name, $value);
-  }
-  // ------------------------  按钮block  ------------------------
+@each $name, $value in $btnSize {
+  @include addBtnSize($name, $value);
+}
 
-  &.is-block {
-    width: 100%;
-  }
-  .is-loading {
-    display: inline-block;
-    animation: load 2s linear infinite;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-  }
-  .is-opacity {
-    opacity: 0;
-  }
+// ------------------------  按钮block  ------------------------
+
+&.is-block {
+  width: 100%;
+}
+
+.is-loading {
+  animation: load 2s linear infinite;
+  position: absolute;
+  left: 50%;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  top: 50%;
+}
+
+.is-opacity {
+  opacity: 0;
+}
 }
 </style>
