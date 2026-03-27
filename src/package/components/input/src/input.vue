d299530be08219b810108c5ac8cf1b5e3b299db7
yutian <nj_yutian@qq.com>
2026-03-27 20:57:24 +0800

    [feat] 优化input组件

--- src/package/components/input/src/input.vue
+++ src/package/components/input/src/input.vue
@@ -1,99 +1,141 @@
-<!-- 
-  @input="aainput"
-  @blur ="aablur"
-  @click ='aaClick'
-  @clear	点击清除按钮的回调	() => void	-	-
-  @parseEnter
- -->
 
 <template>
-  <div :class="[
-    nm.b(),
-    nm.m(size),
-    nm.is('focus', focus),
-    nm.is('disabled', disabled),
-    nm.is('readonly', readonly),
-    nm.is('error', tyFormItem && tyFormItem.formItemError.isShowErrorMsg)
-  ]">
+  <div
+    :class="[
+      nm.b(),
+      nm.m(size),
+      nm.is('focus', focus),
+      nm.is('disabled', disabled),
+      nm.is('readonly', readonly),
+      nm.is('error', tyFormItem && tyFormItem.formItemError.isShowErrorMsg),
+    ]"
+  >
     <!-- 前置元素 -->
     <div :class="nm.e('outPre')" ref="outPre" v-if="useSlots().outPre">
-      <slot name="outPre">
-      </slot>
+      <slot name="outPre"> </slot>
     </div>
     <!-- 前置内容 -->
-    <span :class="nm.e('innerPre')" v-if="useSlots().innerPre" ref="innerPre" :style="{
-      color: disabled ? 'var(--text-4)' : 'var(--text-1)',
-      transform: `translateX(${outPreWidth}px)`
-    }">
+    <span
+      :class="nm.e('innerPre')"
+      v-if="useSlots().innerPre"
+      ref="innerPre"
+      :style="{
+        color: disabled ? 'var(--text-4)' : 'var(--text-1)',
+        transform: `translateX(${outPreWidth}px)`,
+      }"
+    >
       <slot name="innerPre"> </slot>
     </span>
     <!-- 输入框 -->
-    <input v-bind="attrs" v-if="format" v-show="isShowFormat" :class="[
-      nm.is('outPre', outPreWidth > 0),
-      nm.is('outAft', outAftWidth > 0)
-    ]" :style="[
-      {
-        paddingLeft: `${innerPreWidth + 20}px`,
-        paddingRight: `${(innerAftWidth > 0 ? innerAftWidth : 16) +
-          (limitBlockWidth > 0 ? limitBlockWidth - 10 : 0) +
-          20
-          }px`
-      }
-    ]" :disabled="disabled" :readonly="readonly" :value="formatValue" @click="handleToFocus" ref="nativeFormatInp" />
+    <input
+      v-bind="attrs"
+      v-if="format"
+      v-show="isShowFormat"
+      :class="[
+        nm.is('outPre', outPreWidth > 0),
+        nm.is('outAft', outAftWidth > 0),
+      ]"
+      :style="[
+        {
+          paddingLeft: `${innerPreWidth + 20}px`,
+          paddingRight: `${
+            (innerAftWidth > 0 ? innerAftWidth : 16) +
+            (limitBlockWidth > 0 ? limitBlockWidth - 10 : 0) +
+            20
+          }px`,
+        },
+      ]"
+      :disabled="disabled"
+      :readonly="readonly"
+      :value="formatValue"
+      @click="handleToFocus"
+      ref="nativeFormatInp"
+    />
 
-
-    <input :type="attrs.type || 'text'" v-bind="attrs" ref="nativeInp" v-show="!isShowFormat" v-model="model" :class="[
-      nm.is('outPre', outPreWidth > 0),
-      nm.is('outAft', outAftWidth > 0)
-    ]" :style="[
-      {
-        paddingLeft: `${innerPreWidth + 20}px`,
-        paddingRight: `${(innerAftWidth > 0 ? innerAftWidth : 16) +
-          (limitBlockWidth > 0 ? limitBlockWidth - 10 : 0) +
-          20
-          }px`
-      }
-    ]" :disabled="disabled" :readonly="readonly" @input="handleInput" @blur="handleBlur" @click="handleToFocus"
-      @keydown.enter="handleEnter" />
+    <input
+      :type="attrs.type || 'text'"
+      v-bind="attrs"
+      ref="nativeInp"
+      v-show="!isShowFormat"
+      v-model="model"
+      :class="[
+        nm.is('outPre', outPreWidth > 0),
+        nm.is('outAft', outAftWidth > 0),
+      ]"
+      :style="[
+        {
+          paddingLeft: `${innerPreWidth + 20}px`,
+          paddingRight: `${
+            (innerAftWidth > 0 ? innerAftWidth : 16) +
+            (limitBlockWidth > 0 ? limitBlockWidth - 10 : 0) +
+            20
+          }px`,
+        },
+      ]"
+      :disabled="disabled"
+      :readonly="readonly"
+      @input="handleInput"
+      @blur="handleBlur"
+      @click="handleToFocus"
+      @keydown.enter="handleEnter"
+    />
 
     <!-- 后置内容 -->
     <!-- && !isShowClearBtn -->
-    <span ref="innerAft" :class="nm.e('innerAft')" v-if="useSlots().innerAft" :style="{
-      transform: `translateX(-${limitBlockWidth > 0 ? limitBlockWidth + outAftWidth - 10 : outAftWidth
-        }px)`
-    }">
+    <span
+      ref="innerAft"
+      :class="nm.e('innerAft')"
+      v-if="useSlots().innerAft"
+      :style="{
+        transform: `translateX(-${
+          limitBlockWidth > 0 ? limitBlockWidth + outAftWidth - 10 : outAftWidth
+        }px)`,
+      }"
+    >
       <slot name="innerAft"></slot>
     </span>
     <!-- clearAble -->
-    <span v-if="isShowClearBtn" :class="[nm.is('clear')]" :style="{
-      position: 'absolute',
-      right: '10px',
-      top: '0',
-      transform: `translateX(-${limitBlockWidth > 0 ? limitBlockWidth + outAftWidth - 10 : outAftWidth
-        }px)`
-    }" @click.stop="handleClear">
+    <span
+      v-if="isShowClearBtn"
+      :class="[nm.is('clear')]"
+      :style="{
+        position: 'absolute',
+        right: '10px',
+        top: '2px',
+        transform: `translateX(-${
+          limitBlockWidth > 0 ? limitBlockWidth + outAftWidth - 10 : outAftWidth
+        }px)`,
+      }"
+      @click.stop="handleClear"
+    >
+      <TyiCloseLine class="close" size="14"></TyiCloseLine>
+      <TyiCloseCircleLine class="closeCircle" size="14"></TyiCloseCircleLine>
     </span>
 
-    <span :class="nm.is('limit')" v-if="showLimit" ref="limitBlock" :style="{
-      transform: `translateX(-${outAftWidth || 18}px)`
-    }">
+    <span
+      :class="nm.is('limit')"
+      v-if="showLimit"
+      ref="limitBlock"
+      :style="{
+        transform: `translateX(-${outAftWidth || 18}px)`,
+      }"
+    >
       {{ model.length }}/{{ attrs.maxlength }}
     </span>
     <!-- 后置元素 -->
     <div :class="nm.e('outAft')" ref="outAft" v-if="useSlots().outAft">
-      <slot name="outAft">
-      </slot>
+      <slot name="outAft"> </slot>
     </div>
   </div>
 </template>
 <script setup>
-import { inputProps, inputEmits, nm } from './context'
+import { inputProps, inputEmits, nm } from "./context";
 import {
   formContent,
   formItemContent,
-  configProviderDisabled
-} from '../../../hooks/symbolNm'
-import { useCompMvalue } from '../../../hooks/useCompMvalue'
+  configProviderDisabled,
+} from "../../../hooks/symbolNm";
+import { useCompMvalue } from "../../../hooks/useCompMvalue";
 import {
   ref,
   onMounted,
@@ -104,136 +146,132 @@ import {
   useAttrs,
   watch,
   inject,
-  provide
-} from 'vue'
+  provide,
+} from "vue";
+
+import { TyiCloseLine, TyiCloseCircleLine } from "toyaricon";
 
 defineOptions({
-  name: 'TyInput'
-})
-const model = defineModel('modelValue', {
+  name: "TyInput",
+});
+const model = defineModel("modelValue", {
   required: true,
-  type: String
-})
+  type: String,
+});
 
 // 属性
-const attrs = useAttrs()
-const props = defineProps(inputProps)
-const emit = defineEmits(inputEmits)
-const tyForm = inject(formContent, null)
-const tyFormItem = inject(formItemContent, null)
+const attrs = useAttrs();
+const props = defineProps(inputProps);
+const emit = defineEmits(inputEmits);
+const tyForm = inject(formContent, null);
+const tyFormItem = inject(formItemContent, null);
 
 // ref
-const limitBlock = ref()
-const nativeInp = ref()
-const outPre = ref()
-const innerPre = ref()
-const outAft = ref()
-const innerAft = ref()
-const nativeFormatInp = ref()
-const focus = ref(false)
-let outPreWidth = ref(0)
-const formatValue = ref('')
-let limitBlockWidth = ref(0)
-let innerPreWidth = ref(0)
-let outAftWidth = ref(0)
-let innerAftWidth = ref(0)
-let isShowFormatSelf = ref(true)
+const limitBlock = ref();
+const nativeInp = ref();
+const outPre = ref();
+const innerPre = ref();
+const outAft = ref();
+const innerAft = ref();
+const nativeFormatInp = ref();
+const focus = ref(false);
+let outPreWidth = ref(0);
+const formatValue = ref("");
+let limitBlockWidth = ref(0);
+let innerPreWidth = ref(0);
+let outAftWidth = ref(0);
+let innerAftWidth = ref(0);
+let isShowFormatSelf = ref(true);
 
 const isShowFormat = computed(() => {
-  return props.format && isShowFormatSelf.value
-})
-
+  return props.format && isShowFormatSelf.value;
+});
 
 // computed 继承属性
 const disabled = computed(() => {
-  return props.disabled || tyFormItem?.disabled || tyForm?.disabled || false
-})
+  return props.disabled || tyFormItem?.disabled || tyForm?.disabled || false;
+});
 const readonly = computed(() => {
-  return props.readonly || tyFormItem?.readonly || tyForm?.readonly || false
-})
+  return props.readonly || tyFormItem?.readonly || tyForm?.readonly || false;
+});
 const size = computed(() => {
-  return props.size || tyFormItem?.size || tyForm?.size || 'small'
-})
+  return props.size || tyFormItem?.size || tyForm?.size || "small";
+});
 
-const provideInp = reactive({ disabled, readonly })
-provide(configProviderDisabled, provideInp)
+const provideInp = reactive({ disabled, readonly });
+provide(configProviderDisabled, provideInp);
 
 onMounted(() => {
-  outPreWidth.value = outPre?.value?.offsetWidth
-  innerPreWidth.value = innerPre?.value?.offsetWidth
-  outAftWidth.value = outAft?.value?.offsetWidth
-  innerAftWidth.value = innerAft?.value?.offsetWidth
-  limitBlockWidth.value = limitBlock?.value?.offsetWidth || 0
-})
+  outPreWidth.value = outPre?.value?.offsetWidth;
+  innerPreWidth.value = innerPre?.value?.offsetWidth;
+  outAftWidth.value = outAft?.value?.offsetWidth;
+  innerAftWidth.value = innerAft?.value?.offsetWidth;
+  limitBlockWidth.value = limitBlock?.value?.offsetWidth || 0;
+});
 
 function handleInput(event) {
-  limitBlockWidth.value = limitBlock?.value?.offsetWidth || 0
+  limitBlockWidth.value = limitBlock?.value?.offsetWidth || 0;
   // emit('update:modelValue', event.target.value)
-  emit('input', event.target.value)
+  emit("input", event.target.value);
 }
 
 function handleToFocus() {
-  emit('focus')
-  focus.value = true
-  isShowFormatSelf.value = false
+  emit("focus");
+  focus.value = true;
+  isShowFormatSelf.value = false;
   setTimeout(() => {
-    nativeInp.value.focus()
-  })
+    nativeInp.value.focus();
+  });
 }
 
 function handleBlur(event) {
   if (tyForm && tyFormItem && tyFormItem.prop) {
-    tyForm.validate(tyFormItem.prop, 'blur')
+    tyForm.validate(tyFormItem.prop, "blur");
   }
-  emit('blur', event)
-  isShowFormatSelf.value = true
-  focus.value = false
-  if(nativeFormatInp?.value){
-    nativeFormatInp.value.blur()
+  emit("blur", event);
+  isShowFormatSelf.value = true;
+  focus.value = false;
+  if (nativeFormatInp?.value) {
+    nativeFormatInp.value.blur();
   }
 }
 
 function handleClear() {
-  focus.value = false
-  emit('update:modelValue', '')
-  emit('clear')
-
+  focus.value = false;
+  emit("update:modelValue", "");
+  emit("clear");
 }
 
 function handleEnter() {
-  emit('enter', model.value)
+  emit("enter", model.value);
 }
-
-
 
 let isShowClearBtn = computed(() => {
   return (
-    props.modelValue !== '' &&
+    props.modelValue !== "" &&
     props.clearable &&
     !disabled.value &&
     !readonly.value
-  )
-})
+  );
+});
 
 if (props.format) {
-
   watch(
     model,
     (newVal, oldVal) => {
       if (newVal) {
         if (props?.format) {
-          formatValue.value = props.format(newVal)
+          formatValue.value = props.format(newVal);
         } else {
-          formatValue.value = ''
+          formatValue.value = "";
         }
       } else {
-        formatValue.value = ''
+        formatValue.value = "";
       }
     },
-    { immediate: true }
-  )
+    { immediate: true },
+  );
 }
-
 </script>
 
 <style lang="scss" scoped>
@@ -248,6 +286,9 @@ if (props.format) {
   box-sizing: border-box;
   // transition: all var(--time-5);
   overflow: hidden;
+  transition:
+    background-color var(--time-5),
+    border-color var(--time-5); // 统一过渡动画
 
   &__outPre {
     background-color: var(--fill-2);
@@ -314,123 +355,117 @@ if (props.format) {
   }
 
   // ------------------------  input尺寸样式  ------------------------
-  $inputSize: (
-    mini,
-    small,
-    medium,
-    large
-  );
+  $inputSize: (mini, small, medium, large);
 
-@mixin addInputSize($size) {
-  &--#{$size} {
-    height: var(--size-#{$size});
-    line-height: var(--size-#{$size});
-  }
-}
-
-@each $size in $inputSize {
-  @include addInputSize($size);
-}
-
-//  ------------------------  状态  -------------------------------
-.is-clear {
-  height: 100%;
-  top: 0;
-  display: none;
-
-  &:before {
-    font-family: 'toyaricon' !important;
-    font-style: normal;
-    display: inline-block;
-    content: '\eb99';
+  @mixin addInputSize($size) {
+    &--#{$size} {
+      height: var(--size-#{$size});
+      line-height: var(--size-#{$size});
+    }
   }
 
-  &:hover {
-    display: flex;
-    align-items: center;
+  @each $size in $inputSize {
+    @include addInputSize($size);
+  }
 
-    &::before {
-      font-family: 'toyaricon' !important;
-      font-style: normal;
-      display: inline-block;
-      content: '\eb97';
+  //  ------------------------  状态  -------------------------------
+  .is-clear {
+    height: 100%;
+    top: 0;
+    display: none;
+
+
+    .close {
+      display: block;
+    }
+    .closeCircle {
+      display: none;
+    }
+    &:hover {
+      display: flex;
+      align-items: center;
+      .close {
+        display: none;
+      }
+      .closeCircle {
+        display: block;
+      }
       cursor: pointer;
     }
   }
-}
 
-.is-limit {
-  display: flex;
-  align-items: center;
-  position: absolute;
-  height: 100%;
-  top: 0;
-  right: 0;
-  color: var(--text-3);
-  font-size: var(--font-body-1);
-  padding: 0 10px;
-}
-
-&:hover:not(.is-disabled) {
-  background-color: var(--fill-3);
-
-  .ty-input__outPre {
-    border-color: var(--border-color-3);
-    background-color: var(--fill-3);
-  }
-
-  .ty-input__outAft {
-    background-color: var(--fill-3);
-    border-color: var(--border-color-3);
-  }
-
-  input {
-    background-color: var(--fill-3);
-  }
-
-  .is-clear {
+  .is-limit {
     display: flex;
     align-items: center;
+    position: absolute;
+    height: 100%;
+    top: 0;
+    right: 0;
+    color: var(--text-3);
+    font-size: var(--font-body-1);
+    padding: 0 10px;
   }
 
-  .ty-input__innerAft {
-    display: none;
+  &:hover:not(.is-disabled) {
+    background-color: var(--fill-3);
+
+    .ty-input__outPre {
+      border-color: var(--border-color-3);
+      background-color: var(--fill-3);
+    }
+
+    .ty-input__outAft {
+      background-color: var(--fill-3);
+      border-color: var(--border-color-3);
+    }
+
+    input {
+      background-color: var(--fill-3);
+    }
+
+    .is-clear {
+      display: flex;
+      align-items: center;
+    }
+
+    .ty-input__innerAft {
+      display: none;
+    }
   }
-}
 
-&.is-focus:not(.is-disabled) {
-  background-color: unset;
-  border: 1px solid var(--primary-6);
-
-  input {
+  &.is-focus:not(.is-disabled) {
     background-color: unset;
+    border: 1px solid var(--primary-6);
+
+    input {
+      background-color: unset;
+    }
   }
-}
 
-&.is-error {
-  border: 1px solid var(--danger-6);
-}
-
-&.is-disabled {
-  background-color: var(--fill-2);
-  color: var(--text-4);
-
-  input {
-    color: var(--toyar-gray-4);
-    background-color: var(--fill-1);
-    cursor: no-drop;
+  &.is-error {
+    border: 1px solid var(--danger-6);
   }
-}
 
-&.is-readonly {
-  background-color: var(--fill-2);
-  color: var(--text-4);
+  &.is-disabled {
+    background-color: var(--fill-2);
+    color: var(--text-4);
 
-  input {
-    color: var(--toyar-gray-4);
-    background-color: var(--fill-1);
-    // cursor: no-drop;
+    input {
+      color: var(--toyar-gray-4);
+      background-color: var(--fill-1);
+      cursor: no-drop;
+    }
   }
-}
+
+  &.is-readonly {
+    background-color: var(--fill-2);
+    color: var(--text-4);
+
+    input {
+      color: var(--toyar-gray-4);
+      background-color: var(--fill-1);
+      // cursor: no-drop;
+    }
+  }
 }
 </style>
